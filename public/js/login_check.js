af92f8e5f7e1122799e0e6e96adfa4a2ecbf7d73
Steven Kim <stevenkim1989@gmail.com>
2021-01-10 01:29:33 -0800

    fix login button

--- public/js/login_check.js
+++ public/js/login_check.js
@@ -3,11 +3,11 @@ firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
         // User is signed in.
         console.log("User is signed in.");
-        document.getElementById("loginbtn").style.display = "none";
     } else {
         // No user is signed in.
         console.log("User is not signed in.");
         location.href = '../index.html';
+        document.getElementById("loginbtn").style.display = "block";
     }
 });
 
