8339c2b2821bea8c5d74d4cc5a7159fbbd8d6751
Steven Kim <stevenkim1989@gmail.com>
2021-01-10 05:18:47 -0800

    add drawing.html in index.html

---
+++ public/js/index.js
@@ -0,0 +1,13 @@
+let createGame = document.getElementById("createGame");
+let firstPage = document.getElementById("firstPage");
+let secondPage = document.getElementById("secondPage");
+
+function firstShow() {
+    firstPage.style.display = "block";
+    secondPage.style.display = "none";
+}
+
+function secondShow() {
+    firstPage.style.display = "none";
+    secondPage.style.display = "block";
+}
